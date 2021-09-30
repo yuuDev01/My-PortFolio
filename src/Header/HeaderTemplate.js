ac022485c0a4cd0a1fe446e57eacbc6d60d05289
yun <pasenoaz@gmail.com>
2021-09-30 17:41:37 +0900

    20210930 수정1

---
+++ src/Header/HeaderTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeaderTemplate.css';
+const HeaderTemplate  = () =>{
+  return (
+  <div className="title">
+  Hello World
+  </div>
+  );
+}  
+
+export default HeaderTemplate;
